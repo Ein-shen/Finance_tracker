79b528cea5a6a2389f14a1b3d18ba03683d7f74c
Ein-shen <nashgregory9@gmail.com>
2026-08-17 22:38:20 +0800

    fix the router to connect these files client/src/App.jsx  client/src/main.jsx client/src/pages/Home.jsx client/src/pages/user/Account.jsx  client/src/pages/user/Transactions.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-
 import './index.css'
+import { Routes, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Dashboard } from './pages/user/Dashboard'
+import { Login } from './pages/user/Login'
+import { Signup } from './pages/user/Signup'
 
 function App() {
-  
-
   return (
-    <>
-    <div>App</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   )
 }
 
