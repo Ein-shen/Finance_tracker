e72a193af455828346a365fb14494401cd5b796a
Ein-shen <nashgregory9@gmail.com>
2026-08-18 23:37:10 +0800

    added sidebar for not

--- client/src/pages/user/Sidebar.jsx
+++ client/src/pages/user/Sidebar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {User} from 'lucide-react'
 
 export const Sidebar = () => {
   return (
-    <div>
-        <div>
-            
+
+    <div className='flex flex-col'>
+        <div className='h-screen w-48 border-r-2 border-black left-0 items-center text-center space-y-2 p-2'>
+            <h1 className='font-mono text-md border border-2 rounded-md p-2'>
+                schedule 
+            </h1>
+             <h1 className='font-mono text-md  border border-2 rounded-md p-2'>
+                Analytics 
+            </h1>
+             
+             <h1 className='font-mono text-md border border-2 rounded-md p-2'>
+                 Account 
+            </h1>
+
         </div>
     </div>
   )
