36ba455e7fc78419dbc1c39f9fc77ee5277c6869
Ein-shen <nashgregory9@gmail.com>
2026-08-21 18:45:53 +0800

    change the account section

--- client/src/pages/user/Account.jsx
+++ client/src/pages/user/Account.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 export const Account = () => {
   return (
-   <div className="w-full flex flex-row justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 gap-4">
 
-        <h1 className="font-mono text-lg sm:text-2xl theme-text">
-          Account
-        </h1>
+    <div className='w-full'>
+      <div className="w-full flex flex-row justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 gap-4">
+          
+
+              <h1 className="font-mono text-lg sm:text-2xl theme-text">
+                Account
+              </h1>
+
+      </div>
+    
+        
 
 
         <div className='flex flex-col'>
-          <div className='border border-2 rounded-md h-32 w-23 '>
+          <div className=' flex justify-center border border-2 rounded-md h-32 w-23 '>
             
           </div>
 
