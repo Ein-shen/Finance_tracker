9360028e2659da8dd911659f57b08deb30b1731f
Ein-shen <nashgregory9@gmail.com>
2026-08-24 19:52:53 +0800

    added changes to the index removing the balance

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -10,15 +10,8 @@ export const Index = () => {
 
       <div className="grid grid-cols-4 gap-4 mt-6">
 
-        <div className="border border-gray-500 rounded-xl p-5">
-          <p className="text-gray-400">Total Balance</p>
-          <h2 className="text-2xl mt-2">₱25,500.00</h2>
-        </div>
-
-        <div className="border border-gray-500 rounded-xl p-5">
-          <p className="text-gray-400">Total Income</p>
-          <h2 className="text-2xl mt-2">₱40,000.00</h2>
-        </div>
+       
+       
 
         
 
