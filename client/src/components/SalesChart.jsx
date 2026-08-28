d4dbaa285b86add24d8beab77f3a441f03780589
Ein-shen <nashgregory9@gmail.com>
2026-08-28 15:55:15 +0800

    debugging

--- client/src/components/SalesChart.jsx
+++ client/src/components/SalesChart.jsx
@@ -1,37 +1,22 @@
-
-
 import { AreaChart, Area, ResponsiveContainer } from "recharts"
 
 const productsales = [
-     {name: 'shen',
-      product1: 3000,
-      product2: 2000,
-     },
-
-
-     {name: 'hilom',
-      product1: 4000,
-      product2: 1000,
-     },
-
-     {name: 'ouch',
-      product1: 500,
-      product2: 9000,
-     },
-    ]
+  { name: 'shen', product1: 3000, product2: 2000 },
+  { name: 'hilom', product1: 4000, product2: 1000 },
+  { name: 'ouch', product1: 500, product2: 9000 },
+]
 
 export const SalesChart = () => {
   return (
     <ResponsiveContainer width="100%" height="100%">
-    <AreaChart width={500} height={400} data={productsales}>
-        <Area dataKey="product1" 
+      <AreaChart data={productsales}>
+        <Area
+          dataKey="product1"
           type="monotone"
-          datakey="product"
           stroke="#2563eb"
           fill="#3b82f6"
         />
-    </AreaChart>
+      </AreaChart>
     </ResponsiveContainer>
-    
   )
 }
