17466547b3c9a3c153be2e3cde1e0b40dcc0b42a
Ein-shen <nashgregory9@gmail.com>
2026-08-31 21:32:03 +0800

    added border in the dashboard

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -4,7 +4,7 @@ export const AdminDashboard = () => {
   return (
     <div className='flex flex-col items-center'>
       
-      <div className='font-bold text-lg'>
+      <div className='font-bold text-lg border border-2'>
 
         AdminDashboard
       </div>
