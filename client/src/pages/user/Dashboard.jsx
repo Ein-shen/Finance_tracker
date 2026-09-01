aa70e3d5f2dd4d9daa6b8ce10fb17d29258a4720
Ein-shen <nashgregory9@gmail.com>
2026-09-01 12:19:25 +0800

    clean the dashboard

--- client/src/pages/user/Dashboard.jsx
+++ client/src/pages/user/Dashboard.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+
 import { Outlet } from 'react-router-dom'
 import { Leftsidebar } from './Leftsidebar'
-import { Rightsidebar } from './Rightsidebar'
+
 
 export const Dashboard = () => {
   return (
     <div>
       <Leftsidebar />
+     
       
 
       <div className="pt-25 md:pt-20 ml-0 md:ml-64 pt-16 md:pt-8 px-4 sm:px-8 md:px-12 lg:px-20">
