639b1da3dc648655e3584e59b4b9c9022ffccaf3
Ein-shen <nashgregory9@gmail.com>
2026-09-01 20:07:45 +0800

    added changes in the Admin login

--- client/src/pages/admin/Adminlogin.jsx
+++ client/src/pages/admin/Adminlogin.jsx
@@ -19,8 +19,8 @@ export const Adminlogin = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center overflow-y-auto py-8">
-      <div className="p-8 rounded-xl shadow-md w-full max-w-md">
+    <div className="min-h-screen bg-white flex items-center justify-center overflow-y-auto py-8">
+      <div className="bg-[#f3f3f3] text-gray-900 p-8 rounded-xl shadow-md w-full max-w-md border border-gray-200">
 
         {/* Logo / Welcome */}
         <div className="flex flex-col items-center gap-2 mb-6">
@@ -31,7 +31,7 @@ export const Adminlogin = () => {
           />
         </div>
 
-        <h1 className="text-2xl font-mono mb-6 text-center">
+        <h1 className="text-2xl font-mono mb-6 text-center text-gray-900">
           Welcome to Finance Tracker
         </h1>
 
@@ -45,17 +45,15 @@ export const Adminlogin = () => {
         {/* Divider */}
         <div className="flex items-center my-5">
           <hr className="flex-1 border-gray-400" />
-
           <span className="mx-3 text-gray-500 text-sm">
             Continue with Auth
           </span>
-
           <hr className="flex-1 border-gray-400" />
         </div>
 
         <div>
-          <h2 className="text-xl font-mono mb-6 text-center pb-10">
-            Admin
+          <h2 className="text-xl font-mono mb-6 text-center pb-10 text-gray-900">
+            Login as Admin
           </h2>
         </div>
 
@@ -64,27 +62,18 @@ export const Adminlogin = () => {
           type="button"
           onClick={handleGoogle}
           disabled={loading}
-          className="w-full border border-black py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-50 disabled:opacity-50 mt-4"
+          className="w-full border border-black text-gray-900 py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-50 disabled:opacity-50 mt-4"
         >
           <img
             src="https://www.google.com/favicon.ico"
             alt="Google"
             className="w-5 h-5"
           />
-
           Continue with Google
         </button>
 
         {/* Signup */}
-        <p className="text-center text-sm text-gray-500 mt-4">
-          Don't have an account?{' '}
-          <Link
-            to="/signup"
-            className="text-blue-500 hover:underline"
-          >
-            Sign up
-          </Link>
-        </p>
+        
 
       </div>
     </div>
