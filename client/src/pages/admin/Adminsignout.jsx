4b704c53268c2062bf875547ba201849cb183caf
Ein-shen <nashgregory9@gmail.com>
2026-09-01 23:24:44 +0800

    added the signout admin in the upper bar

--- client/src/pages/admin/Adminsignout.jsx
+++ client/src/pages/admin/Adminsignout.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Adminsignout = () => {
   return (
-    <div>Adminsignout</div>
+    <div>
+      <div>
+        <button className='rounded-md border border-2 p-2'>
+          Signout
+        </button>
+      </div>
+    </div>
   )
 }
--- client/src/pages/admin/Upperbar.jsx
+++ client/src/pages/admin/Upperbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Adminsignout } from './Adminsignout'
+
 
 export const Upperbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-16 bg-white border-b border-gray-200 z-50 flex items-center px-6">
-      <div className="font-semibold text-gray-800">
+    <div className="fixed top-0 left-0 w-full h-16 bg-white border-b border-gray-200 z-50 flex items-center px-6 ">
+      <div className="font-semibold text-gray-800 flex flex-row items-center">
         Welcome to Admin Dashboard
+
+
+        <div className='fixed right-0 px-6'>
+            <Adminsignout />
+        </div>
+        
+        
       </div>
     </div>
   )
