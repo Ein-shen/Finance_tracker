888c05367e2b707bdb728697dcdd415490eaa314
Ein-shen <nashgregory9@gmail.com>
2026-09-02 11:00:53 +0800

    added the lucide react signout icons to be more

--- client/src/pages/admin/Adminsignout.jsx
+++ client/src/pages/admin/Adminsignout.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 
 import { signOut, getAuth } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import { LogOut } from 'lucide-react'
+import { User2Icon } from 'lucide-react'
 
 
 export const Adminsignout = () => {
@@ -24,8 +26,10 @@ export const Adminsignout = () => {
       <div>
         <button 
         onClick={handleSignOut}
-        className='cursor-pointer bg-[#f3f3f3] hover:bg-white/10 rounded-md border border-2 p-2'>
-          Signout
+        className='cursor-pointer bg-[#f3f3f3] hover:bg-white/10 rounded-md border border-2 p-2 flex flex-row space-x-1'>
+          <LogOut size={20}/>
+          <User2Icon size={20}/>
+         
         </button>
       </div>
     </div>
