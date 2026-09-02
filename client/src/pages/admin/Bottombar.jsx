e2dfb91dbee05b4853dd5ade949b99e21d683faf
Ein-shen <nashgregory9@gmail.com>
2026-09-02 16:57:50 +0800

    added new changes in the bootombar space in the icons

--- client/src/pages/admin/Bottombar.jsx
+++ client/src/pages/admin/Bottombar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { House } from 'lucide-react'
+import { House, Settings2 } from 'lucide-react'
+import { User } from 'lucide-react'
+import { Settings } from 'lucide-react'
+
 
 export const Bottombar = () => {
   return (
     <div className='w-full  fixed bottom-0 p-4 flex flex justify-center items-center' >
-        <div className='flex flex-row rounded-full border border-2 p-5 space-x-4'>
+        <div className='flex flex-row rounded-full border border-2 p-5 space-x-10'>
             <House />
-            <h1> Bottom bar</h1>
+            <User />
+        
+            <Settings />
         </div>
     </div>
   )
