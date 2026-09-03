524fe046374c9968459b8b79a042e37b812a3c6b
Ein-shen <nashgregory9@gmail.com>
2026-09-03 14:56:53 +0800

    remove the border in the admin dashbaord and the in the settings added the right padding pleasentable in the eyes

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -10,7 +10,7 @@ export const AdminDashboard = () => {
       <Bottombar />
       
       {/* Container with clean top-padding and left-margin for fixed sidebar offset */}
-     <div className="pt-20  h-screen px-4 sm:px-8 md:px-12 lg:px-20 border border-2">
+     <div className="pt-20  h-screen px-4 sm:px-8 md:px-12 lg:px-20">
         <Outlet />
       </div>
     </div>
--- client/src/pages/admin/AdminSettings.jsx
+++ client/src/pages/admin/AdminSettings.jsx
@@ -7,12 +7,12 @@ export const AdminSettings = () => {
   const isLight = theme === 'light'
 
   return (
-    <div className="flex flex-col gap-10">
-      <div className="flex flex-row px-4 sm:px-8 md:px-12 lg:px-20">
+    <div className="flex flex-col gap-10 pt-10 ">
+      <div className="flex flex-row px-4 sm:px-8 md:px-12 lg:px-20 ">
         <h1 className="font-mono text-2xl">Settings</h1>
       </div>
 
-      <div className="pt-10 flex flex-col items-center md:items-start gap-4 px-4 sm:px-8 md:px-12 lg:px-20">
+      <div className="pt-10 flex flex-col items-center md:items-start gap-4 px-4 sm:px-8 md:px-12 lg:px-20 items-center">
         <div className="border-2 rounded-md w-48 flex flex-row p-2 gap-3">
           <span className="flex gap-2 font-mono justify-center md:justify-start">
             {isLight ? (
