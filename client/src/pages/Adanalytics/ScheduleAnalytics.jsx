d5304d88e9dbd54928303a960202241251c7e2c3
Ein-shen <nashgregory9@gmail.com>
2026-09-04 13:16:31 +0800

    added the import to the 3 files inside the Adanalytics

--- client/src/pages/Adanalytics/ScheduleAnalytics.jsx
+++ client/src/pages/Adanalytics/ScheduleAnalytics.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { auth } from '../../firebase'
 
 export const ScheduleAnalytics = () => {
   return (
--- client/src/pages/Adanalytics/UserAnalytics.jsx
+++ client/src/pages/Adanalytics/UserAnalytics.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { auth } from '../../firebase'
+
 
 export const UserAnalytics = () => {
   return (
