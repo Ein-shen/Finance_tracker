b1242e619ed9101f2f35d5b077404982c5a726bf
Ein-shen <nashgregory9@gmail.com>
2026-09-06 17:43:14 +0800

    starting to create the landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full'>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-lg font-bold'>
+          Home page
+        </h1>
+      </div>
+    </div>
   )
 }
