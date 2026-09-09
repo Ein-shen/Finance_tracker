8b229370ab17ade2e1fe3ccd7734ecffa4bf54bc
Ein-shen <nashgregory9@gmail.com>
2026-09-09 15:38:27 +0800

    fix the db.js

--- server/db.js
+++ server/db.js
@@ -1,14 +1,11 @@
 import pg from 'pg'
 import 'dotenv/config'
 
-
 const { Pool } = pg
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: 
-   process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-  
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: true } : false,
 });
 
 export default pool;
