67e1cfa4355ae26c78116e7fe6066e3778d0b1f0
Ein-shen <nashgregory9@gmail.com>
2026-09-09 13:39:47 +0800

    remove netlify origin, no longer used

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ const app = express()
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://expensekontrol.netlify.app',
   process.env.CLIENT_URL,
 ].filter(Boolean)
 
