eff06c477dbf5a8716a5be29cef63eb0bdf4d26b
Ein-shen <nashgregory9@gmail.com>
2026-09-13 22:54:49 +0800

    added change in the support

--- client/src/pages/user/Support.jsx
+++ client/src/pages/user/Support.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Support = () => {
   return (
-    <div>Support</div>
+        <div>
+      <div>
+        <h1 className='font-bold flex justify-center text-center px-10 py-10 text-lg'>
+          This feature will comming soon
+        </h1>
+      
+      </div>
+      </div>
+
   )
 }
