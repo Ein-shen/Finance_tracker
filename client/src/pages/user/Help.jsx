2be0ce7a3ba3710811fa09ed8d67905cec528bce
Ein-shen <nashgregory9@gmail.com>
2026-09-13 22:54:04 +0800

    added change in the help jsx

--- client/src/pages/user/Help.jsx
+++ client/src/pages/user/Help.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Help = () => {
   return (
-    <div>Help</div>
+    <div>
+      <div>
+        <h1 className='font-bold flex justify-center text-center px-10 py-10 text-lg'>
+          This feature will comming soon
+        </h1>
+      
+      </div>
+      </div>
   )
 }
