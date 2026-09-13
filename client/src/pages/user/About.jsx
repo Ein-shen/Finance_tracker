db9971f5a9d2e625b913e6f8868c641e6f7ccf87
Ein-shen <nashgregory9@gmail.com>
2026-09-13 21:53:28 +0800

    added text in about

--- client/src/pages/user/About.jsx
+++ client/src/pages/user/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <h1 className='font-bold flex justify-center text-center px-10 py-10 text-lg'>
+          Comming soon
+        </h1>
+      </div>
+    </div>
   )
 }
