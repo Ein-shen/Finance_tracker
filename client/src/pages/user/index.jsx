59ff1c4d22842a71283a34be65a3be51743b89e6
Ein-shen <nashgregory9@gmail.com>
2026-09-23 22:40:05 +0800

    remove the bg green in the index

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -5,7 +5,7 @@ import { MInusSalary } from "./singlepurpose/MInusSalary"
 export const Index = () => {
   return (
     <div className=" px-4 sm:px-6 md:px-10">
-      <h1 className="text-xl sm:text-2xl bg-green-500 text-theme font-semibold">Financial overview.</h1>
+      <h1 className="text-xl sm:text-2xl text-theme font-semibold">Financial overview.</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 mt-6 pt-10 sm:pt-20">
         <AmountSalary />
